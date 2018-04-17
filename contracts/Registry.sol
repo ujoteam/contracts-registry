8e656b1ef920de5ebb4e1ae1bc76340ddfef0b15
Nihar <ndalal@berkeley.edu>
2018-04-17 11:35:48 -0700

    adds previous block number to event log

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -6,9 +6,10 @@ contract Registry {
     using strings for *;
 
     // todo: choose what to index
-    event LogPublish(address issuer, address subject, bytes32 action, bytes32 contentType, string cid);
+    event LogPublish(address issuer, address subject, bytes32 action, bytes32 contentType, string cid, uint prevblock);
     event LogPublishString(address issuer, string message);
 
+    uint currBlock = block.number;
     // recognized actions == CRUD.
     function publish(address[] _subjects, bytes32[] _actions, bytes32[] _contentTypes, string _cids) public {
         require(_subjects.length == _actions.length && _actions.length == _contentTypes.length);
@@ -16,7 +17,8 @@ contract Registry {
         for (uint8 i = 0; i < _contentTypes.length; i++) {
             string memory cid = prepCids.split("|".toSlice()).toString();
             require(bytes(cid).length != 0);
-            LogPublish(msg.sender, _subjects[i], _actions[i], _contentTypes[i], cid);
+            LogPublish(msg.sender, _subjects[i], _actions[i], _contentTypes[i], cid, currBlock);
+            currBlock = block.number;
         }
     }
 
--- test/registry.js
+++ test/registry.js
@@ -84,4 +84,9 @@ contract('Registry', (accounts) => {
     assert.equal(web3.toAscii(register.logs[1].args.contentType).replace(/\u0000/g, ''), 'MusicGroup');
     assert.equal(register.logs[1].args.cid, 'Qmb');
   });
+  it('record previous block number', async () => {
+    var number = web3.eth.blockNumber;
+    const register = await registry.publish([accounts[1], accounts[1]], ['delete', 'create'], ['MusicGroup', 'MusicGroup'], 'Qma|Qmb', { from: accounts[1] });
+    assert.equal(register.logs[0].args.prevblock, number);
+  })
 });
