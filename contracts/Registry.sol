aa471cb7237f980705ed77fcf4c6577618b342d5
Simon de la Rouviere <simon@delarouviere.com>
2018-04-26 10:53:19 -0400

    Tidied commit

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,29 +1,33 @@
-pragma solidity 0.4.19;
+pragma solidity ^0.4.19;
 import "./utils/strings.sol"; // github.com/Arachnid/solidity-stringutils/strings.sol
 
 
 contract Registry {
     using strings for *;
 
-    // todo: choose what to index
-    event LogPublish(address issuer, address subject, bytes32 action, bytes32 contentType, string cid, uint prevblock);
-    event LogPublishString(address issuer, string message);
+    // solhint-disable-next-line max-line-length
+    event LogPublish(address indexed issuer, address indexed subject, bytes32 action, bytes32 indexed contentType, string cid, uint256 prevBlock);
+    event LogPublishString(address indexed issuer, string message);
 
-    uint currBlock = block.number;
+    uint256 public prevBlock; // zero == first publish
+
     // recognized actions == CRUD.
     function publish(address[] _subjects, bytes32[] _actions, bytes32[] _contentTypes, string _cids) public {
         require(_subjects.length == _actions.length && _actions.length == _contentTypes.length);
+
         var prepCids = _cids.toSlice();
         for (uint8 i = 0; i < _contentTypes.length; i++) {
             string memory cid = prepCids.split("|".toSlice()).toString();
             require(bytes(cid).length != 0);
-            LogPublish(msg.sender, _subjects[i], _actions[i], _contentTypes[i], cid, currBlock);
-            currBlock = block.number;
+            emit LogPublish(msg.sender, _subjects[i], _actions[i], _contentTypes[i], cid, prevBlock);
+            if (prevBlock != block.number) {
+                prevBlock = block.number;
+            }
         }
     }
 
-    // a fallback: generic string publisher in case the the more specific version is not sufficient.
+    // a fallback: generic string publisher in case the the more specific version is not sufficient in future.
     function publishString(string _message) public {
-        LogPublishString(msg.sender, _message);
+        emit LogPublishString(msg.sender, _message);
     }
 }
